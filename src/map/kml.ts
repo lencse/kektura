f6940b0f0e40222dbe646740f77357058096ea9d
Levente Löki <leventeloki@gmail.com>
2019-07-26 01:47:33 +0200

    Code style

--- src/map/kml.ts
+++ src/map/kml.ts
@@ -13,10 +13,7 @@ async function xml2json(xml: string) {
     })
 }
 
-export async function rawFromKml(
-    kmlFilePath: string,
-    areaQualifier: string
-): Promise<string> {
+export async function rawFromKml(kmlFilePath: string, areaQualifier: string): Promise<string> {
     const kml = readFileSync(kmlFilePath).toString()
     const data: any = await xml2json(kml.toString())
     return data.kml
